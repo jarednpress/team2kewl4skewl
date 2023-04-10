4be64515e29d77e5943cc9a5855e926e79288dd1
jonathancsci <97706521+jonathancsci@users.noreply.github.com>
2023-04-10 13:15:49 -0600

    inital setup complete

---
+++ src/init_data/create.sql
@@ -0,0 +1,17 @@
+drop table if exists users cascade;
+create table users(
+    username VARCHAR(50) PRIMARY KEY,
+    password CHAR(60) NOT NULL
+);
+
+drop table if exists songs cascade;
+create table songs(
+    song_id serial primary key,
+    song_name varchar(100) not null,
+    artist_name varchar(100) not null,
+    length_in_sec int not null,
+    weather_label varchar(100) not null
+);
+
+insert into songs (song_name, artist_name, length_in_sec, weather_label) values 
+('Never Be Like You (feat. Kai)', 'Flume', 235, 'Summer');
