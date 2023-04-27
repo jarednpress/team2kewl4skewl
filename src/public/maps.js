da81f65bc875d81d6ce22208f6ca37a090bc5868
Jared Press <japr1166@colorado.edu>
2023-04-27 12:37:46 -0600

    restricted search to US cities

--- src/public/maps.js
+++ src/public/maps.js
@@ -1,8 +1,15 @@
 function initAutocomplete() {
   const input1 = document.getElementById('search-box1');
   const input2 = document.getElementById('search-box2');
-  const autocomplete1 = new google.maps.places.Autocomplete(input1);
-  const autocomplete2 = new google.maps.places.Autocomplete(input2);
+
+  // Add options for city and country restrictions
+  const options = {
+    types: ['(cities)'],
+    componentRestrictions: { country: 'us' },
+  };
+
+  const autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+  const autocomplete2 = new google.maps.places.Autocomplete(input2, options);
 
   autocomplete1.addListener('place_changed', function() {
     const place = autocomplete1.getPlace();
